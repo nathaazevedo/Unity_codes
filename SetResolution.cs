744176f32159fae1ce3f5fec8a2ebe0a9975c619
Nathã <31596001+nathaazevedo@users.noreply.github.com>
2019-05-16 13:44:44 -0300

    Update SetResolution.cs

--- SetResolution.cs
+++ SetResolution.cs
@@ -1,69 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class SetResolution : MonoBehaviour
-{
+public class SetResolution : MonoBehaviour {
+
+    //=======================================
+    //          Resolucoes
+    //=======================================
+    private int[,] resolutions = new int[,] { 
+        {1024, 768},    // 4:3
+        {1280, 720},    // 16:9
+        {1366, 768},    // 16:9
+        {1440, 900},    // 16:10
+        {1600, 900},    // 16:9
+        {1680, 1050},   // 16:10
+        {1920, 1080},   // 16:9
+        {2560, 1080}    // 21:9 ultrawide
+    };
+    //=======================================
+    private Text configText;
+    private int select = 0;
+
     void Start()
     {
-
+        configText = this.gameObject.GetComponent<Text>();
     }
 
     void Update()
     {
+        configText.text = "Set resolution: " + resolutions[ select, 0] + " x " + resolutions[ select, 1];
 
-    }
-    //=======================================
-    // trocar resolucoes
-    //=======================================
-    //      1024 x 768 // 4:3
-    //---------------------------------------
-    //      1280 x 720 // 16:9
-    //      1366 x 768 // 16:9
-    //      1440 x 900 // 16:10
-    //      1600 x 900 // 16:9
-    //      1680 x 1050// 16:10
-    //      1920 x 1080// 16:9
-    //---------------------------------------
-    //      2560 x 1080// 21:9 ultrawide
-    //=======================================
-    public void Resolution01()
-    {
-        Screen.SetResolution(1024, 768, true);
-    }
-    //=======================================
-    public void Resolution02()
-    {
-        Screen.SetResolution(1280, 720, true);
-    }
-    //=======================================
-    public void Resolution03()
-    {
-        Screen.SetResolution(1366, 768, true);
-    }
-    //=======================================
-    public void Resolution04()
-    {
-        Screen.SetResolution(1440, 900, true);
-    }
-    //=======================================
-    public void Resolution05()
-    {
-        Screen.SetResolution(1600, 900, true);
-    }
-    //=======================================
-    public void Resolution06()
-    {
-        Screen.SetResolution(1680, 1050, true);
-    }
-    //=======================================
-    public void Resolution07()
-    {
-        Screen.SetResolution(1920, 1080, true);
-    }
-    //=======================================
-    public void Resolution08()
-    {
-        Screen.SetResolution(2560, 1080, true);
+        if (Input.GetKeyDown("right") && select < (resolutions.Length/resolutions.Rank)-1) // Neste caso, 16/2 = 8 que é o numero de solucoes disponiveis
+        {
+            select++;
+        }
+        else if (Input.GetKeyDown("left") && select > 0)
+        {
+            select--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Screen.SetResolution(resolutions[ select, 0], resolutions[ select, 1], true);
+        } else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 }

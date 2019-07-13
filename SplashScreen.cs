f4c3dfc8b63d0933ee310ca8dccb0fe2e529f070
Nathã <31596001+nathaazevedo@users.noreply.github.com>
2019-07-13 05:01:04 -0300

    Update SplashScreen.cs

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -1,26 +1,28 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms;
 
 // Troca de scene aposto determinado tempo
 // Coloque sua imagem splash na cena
 
-public class SplashScreen : MonoBehaviour {
+public class SplashScreen : MonoBehaviour
+{
 
-  public float duracao = 2;
-  public string cena = "Menu";
-  private float tempo = 0;
-  
-  void Start(){
-//	Screen.SetResolution(1024, 768, true);	  
-  }
-	
-  void Update () {
-    tempo += 1 * Time.deltaTime;
-		if (tempo >= duracao){
-      SceneManager.LoadScene(cena); 
+    public float duracao = 2;
+    public Object loadScene;
+    private float tempo = 0;
+
+    void Start()
+    {
+        //	Screen.SetResolution(1024, 768, true);	  
     }
-  }
-  
+
+    void Update()
+    {
+        tempo += 1 * Time.deltaTime;
+        if (tempo >= duracao)
+        {
+            SceneManager.LoadScene(loadScene.name);
+        }
+    }
+
 }
